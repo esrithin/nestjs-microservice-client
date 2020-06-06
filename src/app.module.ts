42bbc205a8e5ac46f9abb60741bb39434c8670f2
sreethin <sreethin.tblr@gmail.com>
2020-06-06 17:10:35 +0530

    wip

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { Transport, ClientsModule } from '@nestjs/microservices';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+   ClientsModule.register([
+    { name: 'HELLO_SERVICE', transport: Transport.TCP },
+   ]),
+ ],
+controllers: [AppController],
+providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
